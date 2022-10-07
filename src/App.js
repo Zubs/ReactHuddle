0eafc26bcc9a45f29aa689b1aa9aed185759cd78
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-10-07 01:34:38 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Container from "./components/styles/Container.styled";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-      <h1>I love love</h1>
-    </div>
+    <>
+      <Header />
+      <Container>
+        <h1>I love love</h1>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/styles/Header.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+	background-color: #ebfbff;
+	padding: 40px 0;
+`;
+
+export default StyledHeader;
