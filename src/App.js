73016e866784fce56f7f1f16a74846c5904263fb
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-10-07 01:50:47 +0100

    Theme and global styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Container from "./components/styles/Container.styled";
 import Header from "./components/Header";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./components/styles/Globals";
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: 'white',
+    footer: '#003333'
+  }
+};
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={ theme }>
+      <GlobalStyles />
       <Header />
       <Container>
         <h1>I love love</h1>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
